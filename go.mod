406a94856c0ea6d63a853bfb3c06bbd9e9b698f3
Marcelo Aguero <marceloaguero@gmail.com>
2020-09-07 23:24:42 -0300

    Fist ok version

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.15
 
 require (
 	github.com/google/uuid v1.1.2
-	github.com/jinzhu/gorm v1.9.16
+	gorm.io/driver/sqlite v1.1.1
+	gorm.io/gorm v1.20.0
 )
--- main.go
+++ main.go
@@ -6,45 +6,45 @@ import (
 	"time"
 
 	"github.com/google/uuid"
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 // Base contains common columns for all tables.
 type Base struct {
-	ID        uuid.UUID  `gorm:"type:uuid;primary_key;"`
-	CreatedAt time.Time  `json:"created_at"`
-	UpdatedAt time.Time  `json:"update_at"`
-	DeletedAt *time.Time `sql:"index" json:"deleted_at"`
+	ID        uuid.UUID `gorm:"type:uuid;primaryKey"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
 }
 
 // BeforeCreate will set a UUID rather than numeric ID.
-func (base *Base) BeforeCreate(scope *gorm.Scope) error {
-	uuid := uuid.New()
-	return scope.SetColumn("ID", uuid)
+func (base *Base) BeforeCreate(tx *gorm.DB) error {
+	base.ID = uuid.New()
+	return nil
 }
 
 // User is the model for the user table.
 type User struct {
 	Base
-	SomeFlag bool    `gorm:"column:some_flag;not null;default:true" json:"some_flag"`
-	Profile  Profile `json:"profile"`
+	SomeFlag bool `gorm:"not null;default:true"`
+	Profile  Profile
 }
 
 // Profile is the model for the profile table.
 type Profile struct {
 	Base
-	Name   string    `gorm:"column:name;size:128;not null;" json:"name"`
-	UserID uuid.UUID `gorm:"type:uuid;column:user_foreign_key;not null;" json:"-"`
+	Name   string    `gorm:"size:128;not null;"`
+	UserID uuid.UUID `gorm:"type:uuid;column:user_foreign_key;not null;"`
 }
 
 func main() {
-	db, err := gorm.Open("sqlite3", "test.db")
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
 
-	db.LogMode(true)
+	// db.LogMode(true)
 	db.AutoMigrate(&User{}, &Profile{})
 
 	user := &User{SomeFlag: false}
@@ -52,15 +52,16 @@ func main() {
 		log.Panic("Unable to create user.")
 	}
 
-	profile := &Profile{Name: "New User", UserID: user.Base.ID}
+	profile := &Profile{Name: "Marcelo Aguero", UserID: user.Base.ID}
 	if db.Create(&profile).Error != nil {
 		log.Panic("Unable to create profile.")
 	}
 
 	fetchedUser := &User{}
-	if db.Where("id = ?", profile.UserID).Preload("Profile").First(&fetchedUser).RecordNotFound() {
-		log.Panic("Unable to find created user.")
-	}
+	// if db.Where("id = ?", profile.UserID).Preload("Profile").First(&fetchedUser).RecordNotFound() {
+	db.Where("id = ?", profile.UserID).Preload("Profile").First(&fetchedUser)
+	//		log.Panic("Unable to find created user.")
+	//	}
 
 	fmt.Printf("User: %+v\n", fetchedUser)
 }
